c4537d3b5c67e7e0c3d9c97b95c47956da97c718
Adam Pallozzi <adampallozzi@gmail.com>
2022-01-20 07:58:30 +1100

    Add audit log for Collaborators
    (transferred from bullet-train-co/bullet-train-tailwind-css@de03808a77de59ae03f2d9c70472ed2f61eda72a)

--- app/models/activity/version.rb
+++ app/models/activity/version.rb
@@ -7,6 +7,7 @@ class Activity::Version < PaperTrail::Version
   belongs_to :scaffolding_absolutely_abstract_creative_concept, class_name: 'Scaffolding::AbsolutelyAbstract::CreativeConcept', optional: true
   belongs_to :scaffolding_completely_concrete_tangible_thing, class_name: 'Scaffolding::CompletelyConcrete::TangibleThing', optional: true
   belongs_to :action_text_rich_text, class_name: 'ActionText::RichText', optional: true
+  belongs_to :creative_concepts_collaborator, class_name: "Scaffolding::AbsolutelyAbstract::CreativeConcepts::Collaborator", optional: true, foreign_key: :creative_concepts_collaborator_id
 
   before_create do
     method = (item_type.underscore.gsub('/', '_') + '=').to_sym
@@ -17,13 +18,21 @@ class Activity::Version < PaperTrail::Version
     end
 
     # e.g. `self.scaffolding_absolutely_abstract_creative_concept = item`
-    self.send(method, item)
+    begin
+      self.send(method, item)
+    rescue NoMethodError
+      raise "Activity::Version does not respond to #{method}.  You should add #{item.class} as an association to the Activity::Version model."
+    end
 
     if item.is_a? ActionText::RichText
       # The class of the Record that the rich text belongs to
       parent_association = item.record.class.to_s.underscore.gsub("/", "_") + "="
       # Eg scaffolding_completely_concrete_tangible_thing = item.record
       self.send(parent_association, item.record) if self.respond_to? parent_association
+    end
+
+    if scaffolding_absolutely_abstract_creative_concepts_collaborator
+      self.scaffolding_absolutely_abstract_creative_concept ||= scaffolding_absolutely_abstract_creative_concepts_collaborator.creative_concept
     end
 
     if scaffolding_completely_concrete_tangible_thing
@@ -39,5 +48,16 @@ class Activity::Version < PaperTrail::Version
     # TODO - make sure changes to ActionText fields are creating versions correctly
     throw :abort if object_changes.nil?
   end
+
+    # the column name scaffolding_absolutely_abstract_creative_concepts_collaborator is too long for postgres
+    # so that our automatic assignment works, we alias these methods back to the full names
+    def scaffolding_absolutely_abstract_creative_concepts_collaborator=(value)
+      self.creative_concepts_collaborator=(value)
+    end
+
+    def scaffolding_absolutely_abstract_creative_concepts_collaborator
+      creative_concepts_collaborator
+    end
+
 
 end
