8416166aeb756325ad41f56e7fe5ea4439d2855e
Adam Pallozzi <adampallozzi@gmail.com>
2022-01-19 12:40:44 +1100

    Add Rich Text fields to the audit log
    (transferred from bullet-train-co/bullet-train-tailwind-css@cd159adb58626a7f6cb3811cd9fd42335b674683)

--- app/models/activity/version.rb
+++ app/models/activity/version.rb
@@ -6,12 +6,20 @@ class Activity::Version < PaperTrail::Version
   belongs_to :team, class_name: 'Team', optional: true
   belongs_to :scaffolding_absolutely_abstract_creative_concept, class_name: 'Scaffolding::AbsolutelyAbstract::CreativeConcept', optional: true
   belongs_to :scaffolding_completely_concrete_tangible_thing, class_name: 'Scaffolding::CompletelyConcrete::TangibleThing', optional: true
+  belongs_to :action_text_rich_text, class_name: 'ActionText::RichText', optional: true
 
   before_create do
     method = (item_type.underscore.gsub('/', '_') + '=').to_sym
 
     # e.g. `self.scaffolding_absolutely_abstract_creative_concept = item`
     self.send(method, item)
+
+    if item.is_a? ActionText::RichText
+      # The class of the Record that the rich text belongs to
+      parent_association = item.record.class.to_s.underscore.gsub("/", "_") + "="
+      # Eg scaffolding_completely_concrete_tangible_thing = item.record
+      self.send(parent_association, item.record) if self.respond_to? parent_association
+    end
 
     if scaffolding_completely_concrete_tangible_thing
       self.scaffolding_absolutely_abstract_creative_concept ||= scaffolding_completely_concrete_tangible_thing.absolutely_abstract_creative_concept
