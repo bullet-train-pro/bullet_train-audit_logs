d9f5daf1efa4d5467e5458ca7b3fa82dac998f83
Adam Pallozzi <adampallozzi@gmail.com>
2022-01-20 11:45:43 +1100

    Add Activity to actions on show pages, add activity to breadcrumbs, add option to show/hide child objects from activity feed.
    (transferred from bullet-train-co/bullet-train-tailwind-css@d161b9bf8f3519f6dd54a3725b23954e4625e8f0)

--- app/controllers/concerns/activity_actions.rb
+++ app/controllers/concerns/activity_actions.rb
@@ -2,7 +2,11 @@ module ActivityActions
   extend ActiveSupport::Concern
 
   def activity
-    @versions = @child_object.activity_versions.where("item_type != 'Team'").order("id DESC").limit(200).offset(params[:offset].to_i)
+    @versions = @child_object.activity_versions
+    if params[:without_children]
+      @versions = @versions.where(item_type: @child_object.class.to_s)
+    end
+    @versions = @versions.order("id DESC").limit(200).offset(params[:offset].to_i)
     @more_records = @child_object.activity_versions.count > params[:offset].to_i + 200
     @grouped_versions = @versions.group_by { |version| helpers.local_time(version.created_at).to_date }
     respond_to do |format|
