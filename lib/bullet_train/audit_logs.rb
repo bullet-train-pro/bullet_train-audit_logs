195e088fe797d68d6c9b04b21a9921100604c407
Adam Pallozzi <adampallozzi@gmail.com>
2023-01-31 16:15:52 +1100

    Restructure the scaffolders to match other Bullet Train gems

--- lib/bullet_train/audit_logs.rb
+++ lib/bullet_train/audit_logs.rb
@@ -1,5 +1,6 @@
 require "bullet_train/audit_logs/version"
 require "bullet_train/audit_logs/engine"
+require "bullet_train/audit_logs/scaffolders/audit_logs_scaffolder"
 
 module BulletTrain
   module AuditLogs
--- lib/bullet_train/audit_logs/engine.rb
+++ lib/bullet_train/audit_logs/engine.rb
@@ -1,6 +1,37 @@
+require "paper_trail"
 module BulletTrain
   module AuditLogs
     class Engine < ::Rails::Engine
+      # This comes from the paper_trail gem's Railtie. We need to do this because
+      # when we are in a Rails engine, the Railtie is not loaded for a dependent gem
+      initializer "bullet_train.audit_logs", before: "load_config_initializers" do
+        ActiveSupport.on_load(:action_controller) do
+          require "paper_trail/frameworks/rails/controller"
+          include PaperTrail::Rails::Controller
+        end
+
+        ActiveSupport.on_load(:active_record) do
+          require "paper_trail/frameworks/active_record"
+        end
+      end
+
+      initializer "bullet_train.audit_logs.register_routing_concerns" do |app|
+        BulletTrain.routing_concerns << proc do
+          concern :activity do
+            member do
+              get :activity
+            end
+          end
+        end
+      end
+
+      initializer "bullet_train.super_scaffolding.audit_logs.register_scaffolders" do |app|
+        # TODO - suggested improvement to the API:
+        # BulletTrain::SuperScaffolding.register_scaffolder named: "audit_logs", klass: "BulletTrain::AuditLogs::Scaffolders::AuditLogScaffolder"
+        BulletTrain::SuperScaffolding.scaffolders.merge!({
+          "audit_logs" => "BulletTrain::AuditLogs::Scaffolders::AuditLogScaffolder"
+        })
+      end
     end
   end
 end
